1a5cbe6a6cff20cac5afa0e2f58fcb4cecbad773
Lanchon <lanchon@gmail.com>
2016-11-03 14:20:17 -0300

    Create output directory (disabled)

--- src/main/groovy/lanchon/dexpatcher/gradle/tasks/DexpatcherTask.groovy
+++ src/main/groovy/lanchon/dexpatcher/gradle/tasks/DexpatcherTask.groovy
@@ -132,6 +132,17 @@ class DexpatcherTask extends DexpatcherBaseTask {
 
     }
 
+    /*
+    // Gradle automatically creates output directories.
+    @Override protected void beforeExec() {
+        def outDir = getOutputDir()
+        if (outDir) {
+            outDir.mkdirs()
+            if (!outDir.directory) throw new RuntimeException("Could not create directory '$outDir'")
+        }
+    }
+    */
+
     @Override void afterExec() {
         def outFile = getOutputFile()
         if (outFile && !outFile.isFile()) throw new RuntimeException('No output generated')
