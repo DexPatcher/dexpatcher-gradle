90d6fdb98e3862e79ad5911dd2753009376c04e2
Lanchon <lanchon@gmail.com>
2019-03-29 22:14:17 -0300

    Disable currently unused library scopes
    In line with the planned deprecation of the dexpatcher-gradle-tools repo.

--- src/main/groovy/lanchon/dexpatcher/gradle/extensions/DexpatcherConfigExtension.groovy
+++ src/main/groovy/lanchon/dexpatcher/gradle/extensions/DexpatcherConfigExtension.groovy
@@ -27,14 +27,14 @@ class DexpatcherConfigExtension extends AbstractExtension {
     private static final String TOOL_DIR_PROPERTY = PREFIX + 'tool.dir'
     private static final String LIB_DIR_PROPERTY = PREFIX + 'lib.dir'
     private static final String PROVIDED_LIB_DIR_PROPERTY = PREFIX + 'lib.provided.dir'
-    private static final String COMPILE_LIB_DIR_PROPERTY = PREFIX + 'lib.compile.dir'
-    private static final String RUNTIME_LIB_DIR_PROPERTY = PREFIX + 'lib.runtime.dir'
+    //private static final String COMPILE_LIB_DIR_PROPERTY = PREFIX + 'lib.compile.dir'
+    //private static final String RUNTIME_LIB_DIR_PROPERTY = PREFIX + 'lib.runtime.dir'
 
     private static final String DEFAULT_TOOL_SUBDIR_NAME = 'tools'
     private static final String DEFAULT_LIB_SUBDIR_NAME = 'libs'
     private static final String DEFAULT_PROVIDED_SUBDIR_NAME = 'provided'
-    private static final String DEFAULT_COMPILE_SUBDIR_NAME = 'compile'
-    private static final String DEFAULT_RUNTIME_SUBDIR_NAME = 'runtime'
+    //private static final String DEFAULT_COMPILE_SUBDIR_NAME = 'compile'
+    //private static final String DEFAULT_RUNTIME_SUBDIR_NAME = 'runtime'
 
     final ProjectProperties properties
 
@@ -42,14 +42,14 @@ class DexpatcherConfigExtension extends AbstractExtension {
     final DirectoryProperty toolDir
     final DirectoryProperty libDir
     final DirectoryProperty providedLibDir
-    final DirectoryProperty compileLibDir
-    final DirectoryProperty runtimeLibDir
+    //final DirectoryProperty compileLibDir
+    //final DirectoryProperty runtimeLibDir
 
     final Provider<Directory> resolvedToolDir
     final Provider<Directory> resolvedLibDir
     final Provider<Directory> resolvedProvidedLibDir
-    final Provider<Directory> resolvedCompileLibDir
-    final Provider<Directory> resolvedRuntimeLibDir
+    //final Provider<Directory> resolvedCompileLibDir
+    //final Provider<Directory> resolvedRuntimeLibDir
 
     DexpatcherConfigExtension(Project project) {
 
@@ -64,16 +64,16 @@ class DexpatcherConfigExtension extends AbstractExtension {
         libDir.set properties.getAsDirectory(LIB_DIR_PROPERTY)
         providedLibDir = project.layout.directoryProperty()
         providedLibDir.set properties.getAsDirectory(PROVIDED_LIB_DIR_PROPERTY)
-        compileLibDir = project.layout.directoryProperty()
-        compileLibDir.set properties.getAsDirectory(COMPILE_LIB_DIR_PROPERTY)
-        runtimeLibDir = project.layout.directoryProperty()
-        runtimeLibDir.set properties.getAsDirectory(RUNTIME_LIB_DIR_PROPERTY)
+        //compileLibDir = project.layout.directoryProperty()
+        //compileLibDir.set properties.getAsDirectory(COMPILE_LIB_DIR_PROPERTY)
+        //runtimeLibDir = project.layout.directoryProperty()
+        //runtimeLibDir.set properties.getAsDirectory(RUNTIME_LIB_DIR_PROPERTY)
 
         resolvedToolDir = Utils.getResolvedDir(project, toolDir, dir, DEFAULT_TOOL_SUBDIR_NAME)
         resolvedLibDir = Utils.getResolvedDir(project, libDir, dir, DEFAULT_LIB_SUBDIR_NAME)
         resolvedProvidedLibDir = Utils.getResolvedDir(project, providedLibDir, resolvedLibDir, DEFAULT_PROVIDED_SUBDIR_NAME)
-        resolvedCompileLibDir = Utils.getResolvedDir(project, compileLibDir, resolvedLibDir, DEFAULT_COMPILE_SUBDIR_NAME)
-        resolvedRuntimeLibDir = Utils.getResolvedDir(project, runtimeLibDir, resolvedLibDir, DEFAULT_RUNTIME_SUBDIR_NAME)
+        //resolvedCompileLibDir = Utils.getResolvedDir(project, compileLibDir, resolvedLibDir, DEFAULT_COMPILE_SUBDIR_NAME)
+        //resolvedRuntimeLibDir = Utils.getResolvedDir(project, runtimeLibDir, resolvedLibDir, DEFAULT_RUNTIME_SUBDIR_NAME)
 
     }
 
