efd6eb626f4b4a02cdef8173a07792b6852e4636
Lanchon <lanchon@gmail.com>
2019-04-02 17:09:15 -0300

    Gradle 5.1 bug workaround for LazyZipTask (untested)
    See: https://github.com/gradle/gradle/issues/8401

--- src/main/groovy/lanchon/dexpatcher/gradle/tasks/LazyZipTask.groovy
+++ src/main/groovy/lanchon/dexpatcher/gradle/tasks/LazyZipTask.groovy
@@ -10,41 +10,80 @@
 
 package lanchon.dexpatcher.gradle.tasks
 
+import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 
+import lanchon.dexpatcher.gradle.Utils
+
+import org.gradle.api.file.Directory
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.bundling.Zip
-
-// FIXME: This hack apparently does not work on Gradle 5.1 or later.
-// (See: https://github.com/gradle/gradle/issues/8401)
+import org.gradle.util.GradleVersion
 
 @CompileStatic
 class LazyZipTask extends Zip {
 
+    // Conditionally enable workaround for Gradle 5.1 bug (https://github.com/gradle/gradle/issues/8401).
+    private static final boolean GRADLE_5_1 = (GradleVersion.current() >= GradleVersion.version('5.1'))
+
+    // Enable workaround for Gradle < 5.1 unexpected behavior seemingly related to convention implementation.
+    private static final boolean DESTINATION_DIR_WORKAROUND = true
+
+    // Client must configure the output archive only using these properties:
     @Internal final Property<String> lazyArchiveFileName
     @Internal final DirectoryProperty lazyDestinationDirectory
 
     LazyZipTask() {
-        lazyArchiveFileName = project.objects.property(String)
-        lazyArchiveFileName.set project.<String>provider {
-            super.getArchiveName()
+        if (GRADLE_5_1) {
+            // On Gradle >= 5.1 simply alias existing properties.
+            lazyArchiveFileName = gradle_5_1_getArchiveFileName()
+            lazyDestinationDirectory = gradle_5_1_getDestinationDirectory()
+        } else {
+            // On Gradle < 5.1 create new properties and override existing getters.
+            lazyArchiveFileName = project.objects.property(String)
+            lazyArchiveFileName.set project.<String> provider {
+                super.getArchiveName()
+            }
+            lazyDestinationDirectory = project.layout.directoryProperty()
+            if (!DESTINATION_DIR_WORKAROUND) {
+                lazyDestinationDirectory.set project.<Directory> provider {
+                    Utils.getDirectory(project, super.getDestinationDir())
+                }
+            }
         }
-        lazyDestinationDirectory = project.layout.directoryProperty()
-        //lazyDestinationDirectory.set project.<Directory>provider {
-        //    Utils.getDirectory(project, super.getDestinationDir())
-        //}
+    }
+
+    @CompileDynamic
+    private Property<String> gradle_5_1_getArchiveFileName() {
+        return this.getArchiveFileName()
+    }
+
+    @CompileDynamic
+    private DirectoryProperty gradle_5_1_getDestinationDirectory() {
+        return this.getDestinationDirectory()
     }
 
     @Override @Internal public String getArchiveName() {
-        return lazyArchiveFileName.get()
+        if (GRADLE_5_1) {
+            return super.getArchiveName()
+        } else {
+            return lazyArchiveFileName.get()
+        }
     }
 
     @Override @Internal public File getDestinationDir() {
-        //return lazyDestinationDirectory.get().asFile
-        super.destinationDir = lazyDestinationDirectory.get().asFile
-        return super.getDestinationDir()
+        if (GRADLE_5_1) {
+            return super.getDestinationDir()
+        } else {
+            if (!DESTINATION_DIR_WORKAROUND) {
+                return lazyDestinationDirectory.get().asFile
+            } else {
+                super.destinationDir = lazyDestinationDirectory.get().asFile
+                return super.getDestinationDir()
+            }
+        }
     }
 
 }
