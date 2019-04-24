a3d526a7f2d677d5bede4c24eba85de3a1ca452d
Lanchon <lanchon@gmail.com>
2019-04-24 01:47:02 -0300

    Update ApkTool usage comment

--- src/main/groovy/lanchon/dexpatcher/gradle/tasks/AbstractApktoolTask.groovy
+++ src/main/groovy/lanchon/dexpatcher/gradle/tasks/AbstractApktoolTask.groovy
@@ -28,8 +28,8 @@ import org.gradle.api.tasks.PathSensitivity
 abstract class AbstractApktoolTask extends AbstractJavaExecTask {
 
 /*
-Apktool v2.2.1 - a tool for reengineering Android apk files
-with smali v2.1.3 and baksmali v2.1.3
+Apktool v2.4.0 - a tool for reengineering Android apk files
+with smali v2.2.6 and baksmali v2.2.6
 Copyright 2014 Ryszard Wiśniewski <brut.alll@gmail.com>
 Updated by Connor Tumbleson <connor.tumbleson@gmail.com>
 Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
@@ -41,26 +41,30 @@ usage: apktool [-q|--quiet OR -v|--verbose] if|install-framework [options] <fram
  -p,--frame-path <dir>   Stores framework files into <dir>.
  -t,--tag <tag>          Tag frameworks using <tag>.
 usage: apktool [-q|--quiet OR -v|--verbose] d[ecode] [options] <file_apk>
-    --api <API>          The numeric api-level of the file to generate, e.g. 14 for ICS.
- -b,--no-debug-info      don't write out debug info (.local, .param, .line, etc.)
- -f,--force              Force delete destination directory.
- -k,--keep-broken-res    Use if there was an error and some resources were dropped, e.g.
-                         "Invalid config flags detected. Dropping resources", but you
-                         want to decode them anyway, even with errors. You will have to
-                         fix them manually before building.
- -m,--match-original     Keeps files to closest to original as possible. Prevents rebuild.
- -o,--output <dir>       The name of folder that gets written. Default is apk.out
- -p,--frame-path <dir>   Uses framework files located in <dir>.
- -r,--no-res             Do not decode resources.
- -s,--no-src             Do not decode sources.
- -t,--frame-tag <tag>    Uses framework files tagged by <tag>.
+ -api,--api-level <API>   The numeric api-level of the file to generate, e.g. 14 for ICS.
+ -b,--no-debug-info       don't write out debug info (.local, .param, .line, etc.)
+ -f,--force               Force delete destination directory.
+    --force-manifest      Decode the APK's compiled manifest, even if decoding of resources is set to "false".
+ -k,--keep-broken-res     Use if there was an error and some resources were dropped, e.g.
+                          "Invalid config flags detected. Dropping resources", but you
+                          want to decode them anyway, even with errors. You will have to
+                          fix them manually before building.
+ -m,--match-original      Keeps files to closest to original as possible. Prevents rebuild.
+    --no-assets           Do not decode assets.
+ -o,--output <dir>        The name of folder that gets written. Default is apk.out
+ -p,--frame-path <dir>    Uses framework files located in <dir>.
+ -r,--no-res              Do not decode resources.
+ -s,--no-src              Do not decode sources.
+ -t,--frame-tag <tag>     Uses framework files tagged by <tag>.
 usage: apktool [-q|--quiet OR -v|--verbose] b[uild] [options] <app_path>
  -a,--aapt <loc>         Loads aapt from specified location.
  -c,--copy-original      Copies original AndroidManifest.xml and META-INF. See project page for more info.
  -d,--debug              Sets android:debuggable to "true" in the APK's compiled manifest
  -f,--force-all          Skip changes detection and build all files.
+ -nc,--no-crunch         Disable crunching of resource files during the build step.
  -o,--output <dir>       The name of apk that gets written. Default is dist/name.apk
  -p,--frame-path <dir>   Uses framework files located in <dir>.
+    --use-aapt2          Upgrades apktool to use experimental aapt2 binary.
 usage: apktool [-q|--quiet OR -v|--verbose] publicize-resources <file_path>
 
 usage: apktool [-q|--quiet OR -v|--verbose] empty-framework-dir [options]
