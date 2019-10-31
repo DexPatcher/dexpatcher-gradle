23e80d2107b7bfbde2ebb89f6a2f3a8e759e21b3
Lanchon <lanchon@gmail.com>
2019-10-31 18:21:48 -0300

    Add version checks for Gradle and the Android Gradle plugin

--- src/main/groovy/lanchon/dexpatcher/gradle/plugins/AbstractPlugin.groovy
+++ src/main/groovy/lanchon/dexpatcher/gradle/plugins/AbstractPlugin.groovy
@@ -12,11 +12,19 @@ package lanchon.dexpatcher.gradle.plugins
 
 import groovy.transform.CompileStatic
 
+import com.android.builder.model.Version
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.util.GradleVersion
 
 @CompileStatic
 class AbstractPlugin implements Plugin<Project> {
+
+    static final String GRADLE_VERSION_MIN = '4.9'
+    static final String GRADLE_VERSION_MAX = null
+
+    static final String ANDROID_PLUGIN_VERSION_MIN = '3.2'
+    static final String ANDROID_PLUGIN_VERSION_MAX = '3.5'
 
     protected Project project
     protected DexpatcherBasePlugin basePlugin
@@ -26,8 +34,30 @@ class AbstractPlugin implements Plugin<Project> {
         this.project = project
         basePlugin = project.plugins.apply(DexpatcherBasePlugin)
 
+        def requires = 'The configured DexPatcher Gradle plugins require'
+
+        def gradleVer = GradleVersion.current()
+        if (GRADLE_VERSION_MIN && gradleVer < GradleVersion.version(GRADLE_VERSION_MIN)) {
+            throw new RuntimeException("$requires Gradle version $GRADLE_VERSION_MIN or later")
+        }
+        if (GRADLE_VERSION_MAX && gradleVer >= GradleVersion.version(GRADLE_VERSION_MAX)) {
+            throw new RuntimeException("$requires Gradle version earlier than $GRADLE_VERSION_MAX")
+        }
+
+        def androidVer = Version.ANDROID_GRADLE_PLUGIN_VERSION
+        if (ANDROID_PLUGIN_VERSION_MIN && !versionAtLeast(androidVer, ANDROID_PLUGIN_VERSION_MIN)) {
+            throw new RuntimeException("$requires Android Gradle plugin version $ANDROID_PLUGIN_VERSION_MIN or later")
+        }
+        if (ANDROID_PLUGIN_VERSION_MAX && versionAtLeast(androidVer, ANDROID_PLUGIN_VERSION_MAX)) {
+            throw new RuntimeException("$requires Android Gradle plugin version earlier than $ANDROID_PLUGIN_VERSION_MAX")
+        }
+
     }
 
     protected void afterApply() {}
+
+    private static boolean versionAtLeast(String version, String reference) {
+        GradleVersion.version(version) >= GradleVersion.version(reference)
+    }
 
 }
